653859dab36cbd8437d83336a58ae3fb76a749ce
sslushie <86432113+sslushie@users.noreply.github.com>
2024-04-07 16:29:13 -0400

    :)

--- Oregon Trail/src/Fort.java
+++ Oregon Trail/src/Fort.java
@@ -14,11 +14,11 @@ public class Fort {
 	
 	public void visit(Wagon wagon) {
 		this.visited = true;
-		System.out.println("You've arried at" + this.name +". What would you like to do?");
+		System.out.println("You've arried at " + this.name +". What would you like to do?");
 		Scanner scanner = new Scanner(System.in);
 		while(true) {
 			System.out.println("\nOptions:");
-			System.out.println("1.Shop");
+			System.out.println("1. Shop");
 			System.out.println("2. Talk to people");
 			System.out.println("3. Look Around");
 			System.out.println("4. Rest");
@@ -39,8 +39,8 @@ public class Fort {
             case "4":
                 rest(wagon);
                 break;
-				case "5":
-					System.out.println("You continue your journey from the fort.");	
+	    case "5":
+		System.out.println("You continue your journey from the fort.");	
 			}
 		}
 		
