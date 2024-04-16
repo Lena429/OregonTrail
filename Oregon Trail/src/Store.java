7b14aeeb302d92f0edfdbaf85a34e9572c06ca4a
sslushie <sslushie22@gmail.com>
2024-04-16 19:02:11 -0400

    Moving some things from location class to store class

--- Oregon Trail/src/Store.java
+++ Oregon Trail/src/Store.java
@@ -1,4 +1,57 @@
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+public class Store extends Fort {
+	
+	
+	public Store(String name, int miles, Wagon wagon) {
+		super(name, miles, wagon);
+	}
+	
+	 /**
+     * Simulates shopping for items at the fort.
+     * @return A list of equipment items available for sale at the fort
+     */
+	public List<Equipment> shop(){
+		List<Equipment> itemsForSale = new ArrayList<>();
+     	itemsForSale.add(new Food("Apple", 10, 1, true));
+		itemsForSale.add(new Food("Bread", 5, 1, true));
+		itemsForSale.add(new Equipment("Water", 20, 1));
+		return itemsForSale;
+	}
 
-public class Store {
+	
+	 /**
+     * Displays the items available for sale at the fort in a graphical window.
+     */
+	public void displayItemsForSale() {
+        List<Equipment> itemsForSale = shop();
+        JFrame shopWindow = new JFrame("Items for Sale at " + name);
+        shopWindow.setSize(300, 400);
+        shopWindow.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        for (Equipment item : itemsForSale) {
+            JLabel label = new JLabel(item.getName() + ": " + item.getQuantity());
+            JButton Add = new JButton("Add: ");
+            Add.addActionListener(new ActionListener() {
+            	public void actionPerformed (ActionEvent e) {
+            		wagon.addItem(item);
+            	}
+            });
+            panel.add(Add);
+            panel.add(label);
+        }
+
+        shopWindow.add(panel);
+        shopWindow.setVisible(true);
+    }
 
 }
