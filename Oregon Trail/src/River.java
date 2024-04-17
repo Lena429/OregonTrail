6abb12007fff57e545edc104460ecc90db16d285
l-stewart4 <143113359+l-stewart4@users.noreply.github.com>
2024-04-16 22:29:31 -0400

    gggg# WARNING: head commit changed in the meantime
    added txt file and addded stuff to river

--- Oregon Trail/src/River.java
+++ Oregon Trail/src/River.java
@@ -1,4 +1,93 @@
 
-public class River {
+/**
+ * Rivers.java
+ * 
+ * The river class gets the name, height, flow, and width of a river. 
+ *  
+ * @author - Lena Frate
+ * @author - Lillyan Stewart 
+ * @author - Sarah Slusher
+ * @version 1.1.1 - April 16 2024
+ */
+
+
+import java.util.Random;
+import java.util.Scanner;
+import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
+
+
+public class River extends Location {
+	Scanner scr;
+	String river;
+
+	
+	public River(String name, int miles, Wagon wagon) {
+		super(name, miles, wagon);
+		   InputStreamReader isr = new InputStreamReader(this.getClass().getResourceAsStream("OregonTrail/Oregon Trail/src/Rivers.txt"));
+		     scr = new Scanner(isr);
+	}
+	
+	
+
+	
+	// when we do weather we can add parameters to check whether its been raining a bunch or not. 
+	// Also ask estell for chapter 12 rivers
+	// File file = new File("OregonTrail/Oregon Trail/src/Rivers.txt");
+    // Scanner scanner = new Scanner(file);
+        
+  
+
+     //Present options for what they would like to do at the river.
+     //Cross
+     //Wait it out 
+     //Pay your way 
+     //need a money class 
+     
+    public String getRiver() {
+    	String name = scr.nextLine();
+    	return name;
+    
+    }
+	
+	public int getHeight() {
+		int height = scr.nextInt();
+		return height;
+	}
+	
+	public int getFlow() {
+		int flow = scr.nextInt();
+		return flow;	
+	}
+	
+	public int getWidth() {
+		int flow = scr.nextInt();
+		return flow; 
+		
+	}
+	
+	public int random() {
+		//rand number = getradnom number;
+		//maybe I should do math formulas instead
+		Random rnd = new Random();
+		int random = rnd.nextInt(5)+1;
+		switch (random)
+		{		
+		case 1: //remove one ox and reduce speed. 
+		case 2: //remove some food because it was water logged
+		case 3: //drown someone/death. 
+			return 0; 			
+		case 4: //made it across perfectly fine. 
+			return 1;
+		}
+		
+	}
+	
 
 }
+
+
+
