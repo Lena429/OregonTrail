d81a9f19effa5b0aa66b178ff12c647902687f88
Lena429 <l-frate@onu.edu>
2024-04-28 13:55:32 -0400

    initial creation of classes related to health

---
+++ Oregon Trail/src/WagonMember.java
@@ -0,0 +1,15 @@
+/**
+ * WagonMember.java
+ * 
+ * 
+ * @author 
+ * @version
+ */
+public class WagonMember {
+	
+	private String name;
+	
+	public WagonMember(String name) {
+		this.name = name;
+	}
+}
